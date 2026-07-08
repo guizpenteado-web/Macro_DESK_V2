ba1d109f95f77943af0bffd5e3df6074c094a66b
Guilherme Penteado <guizpenteado@gmail.com>
2026-07-08 00:18:41 -0300

    Remove o losango ao lado do logo 'Macro Desk'

--- unified_server.py
+++ unified_server.py
@@ -631,7 +631,7 @@ _SHELL = """<!DOCTYPE html>
 <body>
 
 <nav>
-  <div class="hub-logo">&#9670; <span>Macro Desk</span></div>
+  <div class="hub-logo"><span>Macro Desk</span></div>
   <div class="divider"></div>
 
   <button class="nav-btn active-1" id="btn1" onclick="show(1)">
